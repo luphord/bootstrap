6ff00787b6e86047cd70a5dbe62f6fca87d3b3b9
luphord <luphord@protonmail.com>
2019-11-29 07:46:51 +0100

    list conda envs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,9 @@
 echo 'Checking for conda...'
 command -v conda >/dev/null 2>&1 \
     || { echo >&2 "conda is required, but not installed (or not in path). Aborting."; exit 1; }
-echo 'Conda is available in version' $(conda --version)
+echo 'Conda is available'
+conda --version
+echo
+
+echo 'Available conda environments'
+conda env list
