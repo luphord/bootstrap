0ba9ad55c6c118e60e8dbbcd28d3579963016554
luphord <luphord@protonmail.com>
2019-12-15 19:38:13 +0100

    first steps towards bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
 
+echo "Bootstrapping luphord's dev environment..."
+
+sudo apt update
+sudo apt install -y git
+
+start_pwd=$(pwd)
+tmp_folder="/tmp/luphord$RANDOM"
+mkdir $tmp_folder
+cd $tmp_folder
+echo "Working in $tmp_folder"
+
+git clone https://github.com/luphord/bootstrap.git
+cd bootstrap
+./setup_dev_env.sh
+
+echo "Setup of luphord's dev environment done; deleting temp folder..."
+cd $start_pwd
+rm -rf $tmp_folder
+echo "Setup completed."
