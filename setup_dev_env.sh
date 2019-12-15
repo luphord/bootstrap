6d66533b6f3288ed3ad8eaa19c5a8e5062e9f6ac
luphord <luphord@protonmail.com>
2019-12-15 19:09:18 +0100

    implement git folder/repo check

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -5,7 +5,28 @@ REPOS=${LUPHORD_REPOS:-~/repos}
 REPOS_ENVS_FILE=$(readlink -f repos_envs.txt)
 
 clone_update_repo() {
-    echo "Checking for repository $1"
+    local start_pwd=$(pwd)
+    local repo=$1
+    local repo_folder=$REPOS/$(basename -s .git $repo)
+    echo "----"
+    echo "Checking for repository $repo in $repo_folder..."
+    if [ ! -d $repo_folder ]; then
+        echo "Folder $repo_folder does not exist, cloning repository..."
+        cd $REPOS
+    fi
+    cd $repo_folder
+    if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
+        local remote_url=$(git remote get-url origin)
+        if [ "$repo" = "$remote_url" ]; then
+            echo "Folder $repo_folder is a repository and remote url is correct; pulling..."
+        else
+            echo "Folder $repo_folder is a repository, but remote url is $remote_url, not $repo, please check!"
+        fi
+    else
+        echo "Folder $repo_folder is not a repository, please check!"
+        exit 1
+    fi
+    cd $start_pwd
 }
 
 echo "Installation base folder will be $ROOT"
