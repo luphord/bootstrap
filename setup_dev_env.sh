bbf16161826d9673cc1b59d908d8ffa0cf045295
luphord <luphord@protonmail.com>
2019-12-15 18:42:05 +0100

    read repositories from file and loop over them

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -2,6 +2,16 @@
 
 ROOT=${LUPHORD_ROOT:-~/root}
 REPOS=${LUPHORD_REPOS:-~/repos}
+REPOS_ENVS_FILE=$(readlink -f repos_envs.txt)
+
+clone_update_repo() {
+    echo "Checking for repository $1"
+}
 
 echo "Installation base folder will be $ROOT"
 echo "Repositories will be cloned into $REPOS"
+echo "Reading repositories and environment names from $REPOS_ENVS_FILE"
+
+for repo in $(awk '{print $1}' $REPOS_ENVS_FILE); do
+    clone_update_repo $repo
+done
