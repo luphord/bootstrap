ec33c1bc87f3a4648a6f5bab24182842c7b6dd83
luphord <luphord@protonmail.com>
2019-12-16 06:40:39 +0100

    introduce dry run (take no actual actions)

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -4,6 +4,15 @@ ROOT=${LUPHORD_ROOT:-~/root}
 REPOS=${LUPHORD_REPOS:-~/repos}
 REPOS_ENVS_FILE=$(readlink -f repos_envs.txt)
 
+dry_run() {
+    if [ "$LUPHORD_DRY_RUN" = true ]; then
+        echo "Dry run; skipping action."
+        return 0
+    else
+        return 1
+    fi
+}
+
 setup_conda() {
     echo
     echo 'Checking for conda...'
@@ -27,14 +36,14 @@ clone_update_repo() {
     if [ ! -d $repo_folder ]; then
         echo "Folder $repo_folder does not exist, cloning repository..."
         cd $REPOS
-        git clone $repo
+        dry_run || git clone $repo
     fi
     cd $repo_folder
     if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
         local remote_url=$(git remote get-url origin)
         if [ "$repo" = "$remote_url" ]; then
             echo "Folder $repo_folder is a repository and remote url is correct; pulling..."
-            git pull
+            dry_run || git pull
         else
             echo "Folder $repo_folder is a repository, but remote url is $remote_url, not $repo, please check!"
         fi
