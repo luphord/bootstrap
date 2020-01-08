5de2c37c499bb0fbcf0b7077d49ff964a1b28a42
luphord <luphord@protonmail.com>
2020-01-08 17:08:30 +0100

    task for installing system packages

--- dodo.py
+++ dodo.py
@@ -2,7 +2,13 @@ from pathlib import Path
 
 user = 'luphord'
 user_email = 'luphord@protonmail.com'
+sys_packages_txt = Path(__file__).parent / 'sys_pkgs.txt'
 repos_envs_txt = Path(__file__).parent / 'repos_envs.txt'
+
+def get_sys_packages():
+    with open(sys_packages_txt) as f:
+        for line in f:
+            yield line.strip()
 
 def get_repos_envs():
     '''Load repository and environment names from config file'''
@@ -12,6 +18,16 @@ def get_repos_envs():
             repo = parts[0]
             env = parts[1] if len(parts) > 1 else None
             yield repo, env
+
+def task_install_system_packages():
+    '''Install required system packages via apt'''
+    pkg_string = ' '.join(get_sys_packages())
+    return {
+        'actions': [
+            'sudo apt update',
+            'sudo apt install -y {}'.format(pkg_string)
+        ]
+    }
 
 def task_configure_git():
     '''Configure git user and email address'''
