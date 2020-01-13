4e2ecbbb13524083db4f0bed54385146b0f67c1f
luphord <luphord@protonmail.com>
2020-01-13 06:52:32 +0100

    refactor to use class wrapping repository information

--- dodo.py
+++ dodo.py
@@ -6,12 +6,37 @@ repos_base_folder = Path.home() / 'repos'
 sys_packages_txt = Path(__file__).parent / 'sys_pkgs.txt'
 repos_envs_txt = Path(__file__).parent / 'repos_envs.txt'
 
+
 def get_sys_packages():
     with open(sys_packages_txt) as f:
         for line in f:
             line = line.strip()
             if line:
                 yield line
+
+
+class RepoInfo:
+    '''Meta information about a git repository and its local file paths'''
+    def __init__(self, repo_url, env, repos_base_folder=repos_base_folder):
+        self.url = repo_url
+        self.env = env
+        self.repos_base_folder = Path(repos_base_folder)
+
+    @property
+    def name(self):
+        return Path(self.url).name
+    
+    @property
+    def folder(self):
+        return self.repos_base_folder / Path(self.url).stem
+    
+    @property
+    def dot_git_folder(self):
+        return self.folder / '.git'
+    
+    def __str__(self):
+        return self.url
+
 
 def get_repos_envs():
     '''Load repository and environment names from config file'''
@@ -21,7 +46,11 @@ def get_repos_envs():
             if parts:
                 repo = parts[0]
                 env = parts[1] if len(parts) > 1 else None
-                yield repo, env
+                yield RepoInfo(repo, env)
+
+
+### TASKS ###
+
 
 def task_install_system_packages():
     '''Install required system packages via apt'''
@@ -33,6 +62,7 @@ def task_install_system_packages():
         ]
     }
 
+
 def task_configure_git():
     '''Configure git user and email address'''
     return {
@@ -43,30 +73,30 @@ def task_configure_git():
     }
 
 
-
 def task_clone_repository():
     '''Clone a repository into the specified base folder'''
-    for repo_url, _ in get_repos_envs():
-        repo_folder = repos_base_folder / Path(repo_url).stem
+    for repo_info in get_repos_envs():
         yield {
-            'name': Path(repo_url).name,
-            'actions': ['git clone {} {}'.format(repo_url, repo_folder)],
-            'targets': [repo_folder / '.git'],
+            'name': repo_info.name,
+            'actions': ['git clone {} {}'.format(repo_info.url, repo_info.folder)],
+            'targets': [repo_info.dot_git_folder],
             'uptodate': [True] # up to date if target exists
         }
 
+
 def task_clone_update_repository():
     '''Clone and/or update a git repository'''
-    for repo, _ in get_repos_envs():
+    for repo_info in get_repos_envs():
         yield {
-            'name': Path(repo).name,
-            'actions': ['echo {}'.format(repo)]
+            'name': repo_info.name,
+            'actions': ['echo {}'.format(repo_info)]
         }
+
 
 def task_create_update_conda_env():
     '''Create and/or update a conda environment'''
-    for repo, env in get_repos_envs():
+    for repo_info in get_repos_envs():
         yield {
-            'name': Path(repo).name,
-            'actions': ['echo {} {}'.format(repo, env)]
+            'name': repo_info.name,
+            'actions': ['echo {} {}'.format(repo_info.url, repo_info.env)]
         }
