1037c67e7826a7ae7cdfd01d3dc6221bc1b75c36
luphord <luphord@protonmail.com>
2020-01-27 06:56:27 +0100

    add miniconda/bin to path via .bashrc

--- dodo.py
+++ dodo.py
@@ -124,7 +124,8 @@ def task_install_conda():
     return {
         'actions': [
             'chmod +x {}'.format(miniconda_setup),
-            '{} -b -p {}'.format(miniconda_setup, miniconda_install_folder)
+            '{} -b -p {}'.format(miniconda_setup, miniconda_install_folder),
+            'echo \'PATH="${{PATH}}:{}"; export PATH\' >> ${{HOME}}/.bashrc'.format(miniconda_install_folder / 'bin')
         ],
         'task_dep': ['download_miniconda'],
         'uptodate': ['command -v {}'.format(conda_command)]
